db6bd27481e431d5d2a8cdc48416e33d8bda0e93
Ilgiz <ilgiz1996@mail.ru>
2018-01-21 15:36:31 +0300

    add profile 2

---
+++ src/components/profile/profile_component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Profile extends Component {
+  render () {
+    return (
+      <div>
+        <h1 >
+          Profile!
+        </h1>
+      </div>
+    )
+  }
+}
